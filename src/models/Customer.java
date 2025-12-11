49fb62c701e9ac8bd8ac913ef5c0adb58f9441a8
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-11 14:00:39 +0300

    Add Customer basic fields
    Customer sınıfına name ve idNumber özellikleri eklendi. Constructor ve getter metotları oluşturuldu.

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,7 +1,19 @@
 package models;
 
 public class Customer {
+    private String name;
+    private String idNumber;
 
-	
+    public Customer(String name, String idNumber) {
+        this.name = name;
+        this.idNumber = idNumber;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
 }
