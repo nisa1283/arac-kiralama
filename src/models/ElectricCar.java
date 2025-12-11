4436a2cbd6b76e97821f1da41108742410a20a08
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-11 14:14:59 +0300

    Add getters and setters to ElectricCar and GasCar
    OOP prensiplerine uygun olarak encapsulation güçlendirildi.

--- src/models/ElectricCar.java
+++ src/models/ElectricCar.java
@@ -3,4 +3,12 @@ package models;
 public class ElectricCar {
 	private int batteryCapacity; // kWh
 
+	public int getBatteryCapacity() {
+		return batteryCapacity;
+	}
+
+	public void setBatteryCapacity(int batteryCapacity) {
+		this.batteryCapacity = batteryCapacity;
+	}
+
 }
--- src/models/GasCar.java
+++ src/models/GasCar.java
@@ -3,4 +3,12 @@ package models;
 public class GasCar {
 	private double fuelCapacity; // Litre
 
+	public double getFuelCapacity() {
+		return fuelCapacity;
+	}
+
+	public void setFuelCapacity(double fuelCapacity) {
+		this.fuelCapacity = fuelCapacity;
+	}
+
 }
