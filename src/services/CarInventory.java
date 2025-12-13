3f1a7529aaa5be743a5bbc529cb5fae5ae7c271f
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-13 21:41:46 +0300

    CarInventory temel metotları eklendi
    Araç ekleme ve müsait araçları listeleme fonksiyonları oluşturuldu.

--- src/services/CarInventory.java
+++ src/services/CarInventory.java
@@ -1,6 +1,24 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.Car;
+
 public class CarInventory {
 
-	
+    private List<Car> cars = new ArrayList<>();
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> listAvailableCars() {
+        List<Car> availableCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.isAvailable()) {
+                availableCars.add(car);
+            }
+        }
+        return availableCars;
+    }
 }
