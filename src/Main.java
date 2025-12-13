27c8c1079951545893bbf75067f9f1a6d16c83b8
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-13 21:50:04 +0300

    Main üzerinden console demo eklendi
    Araç ekleme, listeleme ve kiralama işlemleri console çıktısı ile gösterildi.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import models.*;
+import services.*;
 
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        CarInventory inventory = new CarInventory();
+        RentalService rentalService = new RentalService();
 
-	}
+        Car car1 = new ElectricCar("E1", "Tesla", "Model 3", 2022, 75);
+        Car car2 = new GasCar("G1", "Toyota", "Corolla", 2020, 50);
 
+        inventory.addCar(car1);
+        inventory.addCar(car2);
+
+        System.out.println("Müsait araçlar:");
+        for (Car car : inventory.listAvailableCars()) {
+            System.out.println(car);
+        }
+
+        rentalService.rentCar(car1);
+
+        System.out.println("\nKiralama sonrası müsait araçlar:");
+        for (Car car : inventory.listAvailableCars()) {
+            System.out.println(car);
+        }
+    }
 }
