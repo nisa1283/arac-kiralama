dc940de4ca7db325544a104efdfdc751a434f404
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-13 21:36:22 +0300

    GasCar constructor ve rental fee eklendi
    GasCar için kalıtım tamamlandı ve calculateRentalFee override edildi.

--- src/models/GasCar.java
+++ src/models/GasCar.java
@@ -1,14 +1,36 @@
 package models;
 
-public class GasCar {
-	private double fuelCapacity; // Litre
+public class GasCar extends Car {
 
-	public double getFuelCapacity() {
-		return fuelCapacity;
+    private double fuelCapacity; // Litre
+
+    public GasCar(String id, String brand, String model, int year, double fuelCapacity) {
+        super(id, brand, model, year);
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    @Override
+    public double calculateRentalFee(int days) {
+        return days * 200;
+    }
+
+	@Override
+	public void rent() {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public void setFuelCapacity(double fuelCapacity) {
-		this.fuelCapacity = fuelCapacity;
+	@Override
+	public void returnVehicle() {
+		// TODO Auto-generated method stub
+		
 	}
-
 }
