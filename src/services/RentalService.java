3dd959d6f70b57a36607c309e7061a1cf4475455
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-13 21:42:31 +0300

    RentalService kiralama ve iade eklendi
    Araç kiralama ve iade işlemleri için servis katmanı oluşturuldu.

--- src/services/RentalService.java
+++ src/services/RentalService.java
@@ -1,6 +1,16 @@
 package services;
 
+import models.Car;
+
 public class RentalService {
 
-	
+    public void rentCar(Car car) {
+        if (car.isAvailable()) {
+            car.setAvailable(false);
+        }
+    }
+
+    public void returnCar(Car car) {
+        car.setAvailable(true);
+    }
 }
