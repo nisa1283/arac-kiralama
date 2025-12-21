b6580db524c8039c55cba53faf415c41c81aab82
Nisa Ünal <nisanurunal250@gmail.com>
2025-12-21 17:10:49 +0300

    Add RentalRecord model
    Kiralama bilgilerini tutmak için RentalRecord sınıfı eklendi.

---
+++ src/models/RentalRecord.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.time.LocalDate;
+
+public class RentalRecord {
+
+    private Car car;
+    private String customerName;
+    private int rentalDays;
+    private LocalDate rentalDate;
+
+    public RentalRecord(Car car, String customerName, int rentalDays) {
+        this.car = car;
+        this.customerName = customerName;
+        this.rentalDays = rentalDays;
+        this.rentalDate = LocalDate.now();
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public LocalDate getRentalDate() {
+        return rentalDate;
+    }
+}
